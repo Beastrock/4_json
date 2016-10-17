e0ff4f91d2bab1793e86c5e19fc945769116bc97
beastrock <beastrock@mail.ru>
2016-10-17 21:18:38 +0000

    пеп8

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,20 @@
 import json
+import os.path
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding="UTF-8" ) as handle:
-        data = json.load(handle)
-        return data
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding="UTF-8") as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    print(json.dumps(data, indent=4, sort_keys=True, separators=(',', ':'), ensure_ascii=False))
+    print(json.dumps
+          (data, indent=4, sort_keys=True, separators=(',', ':'), ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    data = input("введите путь до файла!n/")
-    pretty_print_json(load_data(data))
+    json_filepath = input \
+        ("Введите путь до файла или сам файл, если он находится в этой директории:\n")
+    pretty_print_json(load_data(json_filepath))
